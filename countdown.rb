fce10db151a9ceee63ae341b720f8257e4524e30
Samuel <sblanc613@gmail.com>
2020-12-17 23:21:31 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_midnight)
+   
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -=1
+    end
+    return "HAPPY NEW YEAR!"
+
+end
+
+
+def countdown_with_sleep(timer)
+
+    while timer > 0
+        puts "#{timer} SECOND(S)!"
+        sleep 5
+        timer -=1
+    end
+    return "HAPPY NEW YEAR!"
+
 end
